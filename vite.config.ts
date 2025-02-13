c657e977d26e5187c5cf904b7f34e42c02302d8c
johnmel <iamjohnmelsulla@gmail.com>
2025-02-13 20:32:12 +0800

    update vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 5174, // Change this to your desired port
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("lodash")) return "lodash-vendor";
-            return "vendor"; // Separate other dependencies
-          }
-        },
-      },
-    },
-  },
+  }
 });
