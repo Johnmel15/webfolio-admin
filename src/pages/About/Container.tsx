15d35a51e9c90b2474710ff5bfd7cd55ab0f300e
Johnmel <iamjohnmelsulla@gmail.com>
2025-02-14 17:45:02 +0800

    remove WYSIWYGEditor and change to textarea instead. add custom favicon

--- src/pages/About/Container.tsx
+++ src/pages/About/Container.tsx
@@ -1,6 +1,4 @@
 import { FC, useState } from "react";
-// import { AboutTable } from "./components";
-import WYSIWYGEditor from "../../components/ui/WYSIWYGEditor";
 
 const Container: FC = () => {
   const [content, setContent] = useState("");
@@ -8,13 +6,32 @@ const Container: FC = () => {
     <div className="flex flex-col items-start">
       {/* <AboutTable /> */}
       <div className="bg-white shadow-md rounded-lg p-6 w-full">
-      <h1 className="text-xl font-bold mb-2">React Simple WYSIWYG</h1>
-      <WYSIWYGEditor value={content} onChange={setContent} />
-      <div className="mt-4 p-2 border">
-        <h2 className="text-lg font-semibold">Preview:</h2>
-        <div dangerouslySetInnerHTML={{ __html: content }} />
+        <h1 className="text-xl font-bold mb-2">About Section</h1>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col items-start">
+            <label htmlFor="for-header">Header:</label>
+            <input
+              type="text"
+              id="for-header"
+              className="border rounded-md p-2 w-full"
+            />
+          </div>
+          <div className="flex flex-col items-start">
+            <label htmlFor="for-header">Content:</label>
+            <textarea
+              className="w-full p-2 border rounded-md"
+              style={{ height: "200px" }}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-end">
+            <button className="bg-purple-700 hover:bg-purple-800 text-white px-4 py-2 rounded-md">
+              Save
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/pages/About/components/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as AboutTable } from "./Table";
