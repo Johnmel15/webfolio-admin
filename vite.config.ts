9ac3df2ea244920f573e9633c7f6d2a377f1e076
johnmel <iamjohnmelsulla@gmail.com>
2025-02-16 22:51:43 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     host: true,
     port: 5174, // Change this to your desired port
